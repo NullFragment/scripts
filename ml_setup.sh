6a8cf091699a0ad2973d653a6294b135f413ba0f
Kyle Salitrik <nullfragment@gmail.com>
2017-06-30 14:56:34 -0400

    Added script to install ML dependencies for virtualbox Ubuntu 16.04

---
+++ ml_setup.sh
@@ -0,0 +1,53 @@
+#! /bin/bash
+echo "Please allow sudo to do the work for you..."
+sudo echo "Setting Temp Downloads directory"
+install -d ~/Downloads/temp
+cd ~/Downloads/temp
+
+bash_pkgs=(
+	'zsh'
+	'xterm'
+	'tmux'
+	'vim'
+	'htop'
+	'git'
+	'python3'
+	'python3-pip'
+	'python3-tk'
+	'python3-numpy'
+	'openmpi-bin'
+	)
+
+py_pkgs=(
+	'http://download.pytorch.org/whl/cu75/torch-0.1.12.post2-cp35-cp35m-linux_x86_64.whl'
+	'torchvision'
+	'tensorflow'
+	'keras'
+	'matplotlib'
+	)
+
+echo "Updating package repositories"
+sudo apt-get -qq update 
+
+echo "Upgrading installed packages"
+sudo apt-get -qq upgrade -y 
+
+echo "Installing requested packages"
+for i in "${bash_pkgs[@]}"
+do
+	sudo -H apt-get -qq install -y $i 
+done
+
+echo "Ensuring pip is up-to-date"
+sudo -H pip3 install -qq --upgrade pip 
+
+echo "Installing python ML packages"
+for i in "${py_pkgs[@]}"
+do
+	sudo -H pip3 install -qq $i 
+done
+
+echo "Cleaning up unnecessary packages..."
+sudo apt-get -qq update 
+sudo apt -qq autoremove -y 
+echo "Finished"
